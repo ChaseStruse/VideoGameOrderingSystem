430d1d36f664c81880c945c3fe6fa84049c0b41b
Chase <chasestruse@gmail.com>
2020-09-05 20:33:49 -0500

    Added project structure

---
+++ VideoGameOrderingSystem/VideoGameOrderingSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VideoGameOrderingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
